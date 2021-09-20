ad6be348643e86a4d316c75a461b325bcc8b9919
ebidora <ebidora77@gmail.com>
2021-09-20 21:45:07 +0900

    create delete edit login

--- ebi/ebi_todo/urls.py
+++ ebi/ebi_todo/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete,CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('',TaskList.as_view(),name='tasks')
+    path('login/',CustomLoginView.as_view(), name='login'),
+    path('loout/',LogoutView.as_view(next_page='login'), name='loout'),
+    
+    path('',TaskList.as_view(),name='tasks'),
+    path('<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('create-task/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>',TaskDelete.as_view(),name='task-delete'),
 ]
